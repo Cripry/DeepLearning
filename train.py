c5c3536e31f327b51456e4c5c67c4c49da913d3d
cripry <cristian.preguza@gmail.com>
2024-12-26 13:36:03 +0200

    renamed folder, and trained model for first time

--- train.py
+++ train.py
@@ -4,22 +4,19 @@ import numpy as np
 import json
 import sys
 
-directory = "./L4/data2test"
+directory = "./L4/data"
 user_data = directory
 valid_data = directory
-test_data = (
-    directory + "/test"
-)  # this can be the label book, or any other test set you create
+test_data = directory + "/test" # this can be the label book, or any other test set you create
 
 ### DO NOT MODIFY BELOW THIS LINE, THIS IS THE FIXED MODEL ###
-
 batch_size = 8
 tf.random.set_seed(123)
 
 
 if __name__ == "__main__":
     train = tf.keras.preprocessing.image_dataset_from_directory(
-        user_data + "/train",
+        user_data + '/train',
         labels="inferred",
         label_mode="categorical",
         class_names=["i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x"],
@@ -30,7 +27,7 @@ if __name__ == "__main__":
     )
 
     valid = tf.keras.preprocessing.image_dataset_from_directory(
-        user_data + "/val",
+        user_data + '/val',
         labels="inferred",
         label_mode="categorical",
         class_names=["i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x"],
